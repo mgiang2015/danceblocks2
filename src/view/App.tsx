130db8232638db7e2a6851885d9b1a924152a63a
Le Minh Giang <mgiang2015@gmail.com>
2023-01-14 02:22:53 +0800

    Attempts to change logo

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -3,8 +3,7 @@ import BlockingList from './components/blockingList/blockingList';
 import Toolbar from './components/toolbar/toolbar';
 import Stage from './components/stage/stage';
 import DancerList from './components/dancerList/dancerList';
-import { useEffect, useState } from 'react';
-import { getWindowDimensions } from '../model/util';
+import { useEffect } from 'react';
 import Stage3d from './components/stage3d/stage3d';
 import { useAppSelector } from '../control/hooks';
 import { selectView3d } from '../control/stateSlice';
@@ -13,6 +12,9 @@ const padding = '0.5em'
 
 function App() {
   const view3d = useAppSelector(selectView3d);
+  useEffect(() => {
+    document.title = 'Danceblocks 2';
+  }, []);
 
   return (
     <div className="App">
