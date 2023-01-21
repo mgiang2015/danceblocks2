57ce623331317e64e81969d334200ce143493a86
Le Minh Giang <mgiang2015@gmail.com>
2023-01-21 13:55:27 +0700

    Change dancer angle on 2d stage

--- src/view/components/dancerList/dancerListItem.tsx
+++ src/view/components/dancerList/dancerListItem.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from './dancerList.module.css'
 import { useAppDispatch } from "../../../control/hooks";
-import { renameDancer, changeDancerColor, deleteDancer } from "../../../control/stateSlice";
+import { renameDancer, changeDancerColor, deleteDancer, changeDancerAngle } from "../../../control/stateSlice";
 
 interface DancerListItemProp {
     dancer: Dancer
@@ -25,7 +25,11 @@ export default function DancerListItem({ dancer }: DancerListItemProp): JSX.Elem
     const onColorChange = (event: React.FormEvent<HTMLInputElement>) => {
         if (event.currentTarget.value !== dancer.color) {
             changeColor(event.currentTarget.value);
-        }        
+        }
+    }
+
+    const onAngleChange = (event: React.FormEvent<HTMLInputElement>) => {
+        changeAngle(parseFloat(event.currentTarget.value));
     }
 
     const rename = (newName: string) => {
@@ -37,6 +41,10 @@ export default function DancerListItem({ dancer }: DancerListItemProp): JSX.Elem
     const removeDancer = () => {
         dispatch(deleteDancer({ id: dancer.id }));
     }
+    
+    const changeAngle = (newAngle: number) => {
+        dispatch(changeDancerAngle({ id: dancer.id, angle: newAngle}))
+    }
 
     const onSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
         if (name !== "" && name !== dancer.name) {
@@ -46,61 +54,46 @@ export default function DancerListItem({ dancer }: DancerListItemProp): JSX.Elem
     }
     
     return (
-        <div className={styles.container}>
-            <div>
-                <p onClick={() => setEdit(!edit)} className={styles.gridItem}>{dancer.name}</p>
+        <div className={styles.parentContainer}>
+            <div className={styles.infoContainer}>
+                <div>
+                    <p onClick={() => setEdit(!edit)} className={styles.gridItem}>{dancer.name}</p>
+                </div>
+                <div>
+                    <div style={{ height: '1em', width: '1em', backgroundColor: dancer.color }}/>
+                </div>
             </div>
-            <div>
-                <div style={{ height: '1em', width: '1em', backgroundColor: dancer.color }}/>
-            </div>
             {
-                edit
-                ? 
-                <div>
-                    <p className={styles.gridItem}>Name</p>
+                edit ?
+                <div className={styles.formContainer}>
+                    <div>
+                        <p>Name</p>
+                    </div>
+                    <div>
+                        <input type={"text"} onChange={onNameChange} defaultValue={dancer.name} />
+                    </div>
+                    <div>
+                        <p>Color</p>
+                    </div>
+                    <div>
+                        <input type={"color"} defaultValue={dancer.color} onChange={onColorChange} />
+                    </div>
+                    <div>
+                        <p>Angle</p>
+                    </div>
+                    <div>
+                        <input type={"number"} defaultValue={dancer.angle || 0} onChange={onAngleChange} />
+                    </div>
+                    <div>
+                        <button type="button" onClick={onSubmit}>Update</button>
+                    </div>
+                    <div>
+                        <button type="button" onClick={removeDancer}>Delete</button>
+                    </div>
                 </div>
                 : null
             }
-            {
-                edit
-                ? 
-                <div>
-                    <input type={"text"} value={name} onChange={onNameChange} />
-                </div>
-                : null
-            }
-            {
-                edit
-                ?
-                <div>
-                    <p className={styles.gridItem}>Color</p>
-                </div>
-                : null
-            }
-            {
-                edit
-                ?
-                <div>
-                    <input type={"color"} value={dancer.color} onChange={onColorChange} />
-                </div>
-                : null
-            }
-            {
-                edit
-                ?
-                <div>
-                    <button type="button" onClick={onSubmit}>Update</button>
-                </div>
-                : null
-            }
-            {
-                edit
-                ?
-                <div>
-                    <button type="button" onClick={removeDancer}>Delete</button>
-                </div>
-                : null
-            }
+            
         </div>
     )
 }
--- src/view/components/stage/stageDancer.tsx
+++ src/view/components/stage/stageDancer.tsx
@@ -18,7 +18,7 @@ export default function StageDancer({ dancer }: DancerProps) {
     return (
         <div className={styles.dancerContainer} draggable={true} onDragStart={(e) => onDragStart(e)} style={{ position: "absolute", top: dancer.yCoord, left: dancer.xCoord, transform: `translate(-50%, -75%)`, display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: "pointer" }}>
             <p style={{ margin: 0 }}>{`${dancer.name}`}</p>
-            <div style={{ position: "relative", height: '2.25em', width: '1.5em', transform: 'rotate(0deg)' }}>
+            <div style={{ position: "relative", height: '2.25em', width: '1.5em', transform: `rotate(${dancer.angle || 0}deg)` }}>
                 <div style={{ position: "absolute", top: 0, left: 0 ,backgroundColor: dancer.color, height: '1.5em', width: '1.5em', borderRadius: '50%' }} />
                 <div style={{ position: "absolute", top: '1.5em', left: 0, width: 0, height: 0, borderLeft: '0.75em solid transparent', borderRight: '0.75em solid transparent', borderTop: `0.75em solid ${dancer.color}` }} />
             </div>
